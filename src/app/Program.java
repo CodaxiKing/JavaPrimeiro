7359ceaaa6d171841ba494046eb030a971a64c67
Diego Tavares <Diegomelo437@gmail.com>
2024-10-01 18:36:14 -0300

    Aula sobre Println, PrintF e Print, além de aprender concatenação

---
+++ src/app/Program.java
@@ -0,0 +1,20 @@
+package app;
+
+import java.util.Locale;
+
+public class Program {
+
+    public static void main(String[] args) {
+        double x = 10.35784;
+        Locale.setDefault(Locale.US);
+        System.out.printf("%.2f%n", x);
+        System.out.println("Resultado = " + x + " Metros");
+
+        String nome = "Maria";
+        int idade = 31;
+        double renda = 4000.0;
+
+        System.out.printf("%s tem %d anos e ganha R$ %.2f reais%n", nome, idade, renda);
+
+    }
+}
